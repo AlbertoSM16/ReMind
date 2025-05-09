6e4068fef7fe55fd8bfba85f276b2ae28d89bd15
Alberto Siles <albertosiles2001@gmail.com>
2025-05-09 09:58:11 +0200

    landingPage done

--- front-end/ReMind/src/app/app.component.ts
+++ front-end/ReMind/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
+import { LadingPageComponent } from './pages/lading-page/lading-page.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AuthComponent],
+  imports: [RouterOutlet, AuthComponent, LadingPageComponent],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ front-end/ReMind/src/app/pages/lading-page/lading-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lading-page',
+  imports: [],
+  templateUrl: './lading-page.component.html',
+  styleUrl: './lading-page.component.css'
+})
+export class LadingPageComponent {
+  linea1: string = 'Una rutina para la mente, un';
+  linea2: string = 'apoyo para';
+}
